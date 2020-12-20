6758e0c1cccc9dbc14a24eb30f0616c0ceea9029
Sarah Massie <smassie@my365.bellevue.edu>
2020-12-19 17:40:45 -0800

    Exercise 1.3

---
+++ week-1/hello-world/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'hello-world';
+  myWorld = "You are now in Sarah's world!"
+}
