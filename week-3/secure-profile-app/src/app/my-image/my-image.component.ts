8cd83ae988bc2d35ed442dab467d5c7d4627dcdd
Sarah Massie <smassie@my365.bellevue.edu>
2021-01-14 16:06:30 -0800

    master

---
+++ week-3/secure-profile-app/src/app/my-image/my-image.component.ts
@@ -0,0 +1,22 @@
+/*
+ * Title: my-image.component.ts
+ * Author: Sarah Massie
+ * Date: 9 January 2021
+ * Description: component for my image
+*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-image',
+  templateUrl: './my-image.component.html',
+  styleUrls: ['./my-image.component.css']
+})
+export class MyImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
